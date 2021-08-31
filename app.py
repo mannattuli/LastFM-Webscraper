8d988d0658c1c7954ad556914ea145deb6147ec4
Mannat <84506745+MannatTuli@users.noreply.github.com>
2021-08-31 14:36:58 +0530

    adding images

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
+import requests 
 import bs4
-import requests
-from flask import Flask
-from flask import request
-from flask import render_template
 
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return  render_template("index.html")
-
-result = requests.get("https://www.last.fm/user/")
+result = requests.get("https://www.last.fm/user/mxnnxtz")
 soup = bs4.BeautifulSoup(result.text, "lxml")
 
 songRightNow = soup.select(".chartlist-name")[0]
@@ -19,18 +10,25 @@ print(songRightNow.text.replace("\n", ""))
 artistRightNow = soup.select(".chartlist-artist")[0]
 print(artistRightNow.text.replace("\n", ""))
 
-file = open("templates/index.html","r+")
+image = soup.select('img')[4]
+links = image['src']
+print(image)
+
+file = open("index.html","r+")
 file.truncate(0)
 file.close()
 
-with open("templates/index.html", "a") as f:
+with open("index.html", "a") as f:
 	f.write('<html>')
 	f.write('<head>')
 	f.write('<title>Songs</title>')
 	f.write('</head>')
 	f.write('<body style="background-color: #121212; color: wheat">')
-	f.write('<h1>Songs</h1>')
+	f.write('<h1>Song</h1>')
 	f.write("Currently listening to: " + songRightNow.text.replace("\n", ""))
 	f.write("<br> By: " + artistRightNow.text.replace("\n", ""))
+	f.write("<br>")
+	f.write('<img src="' + links + '" style="height:100px; width:100px">')
 	f.write('</body>')
 	f.write('</html>')
+	file.close()
